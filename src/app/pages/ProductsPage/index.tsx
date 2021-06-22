004883c279d8fadef012b0ba9f7933c539f2b9f3
Johannes Stenberg <johannes.stenberg@solteq.com>
2021-06-22 16:14:42 +0300

    products sorting

--- src/app/pages/ProductsPage/index.tsx
+++ src/app/pages/ProductsPage/index.tsx
@@ -12,14 +12,26 @@ import { CurrencyFormatter } from 'utils/formatters';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { useTranslation } from 'react-i18next';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 interface Params {
   category?: ProductCategory;
   search?: string;
 }
 
+enum SortByEnum {
+  Default,
+  Expensive,
+  Cheap,
+  Alphabetical,
+}
+
 export function ProductsPage() {
   const { category, search } = useParams<Params>();
+
+  const [sortBy, setSortBy] = React.useState<SortByEnum>(SortByEnum.Default);
 
   const { t } = useTranslation();
 
@@ -30,9 +42,26 @@ export function ProductsPage() {
       ? p.category === category.valueOf()
       : search
       ? p.title.toLowerCase().includes(search.toLowerCase()) ||
-        p.category.toLowerCase().includes(search.toLowerCase())
+        ProductCategoryToString(p.category, t)
+          .toLowerCase()
+          .includes(search.toLowerCase())
       : true,
   );
+
+  switch (sortBy) {
+    case SortByEnum.Alphabetical:
+      products.sort((a, b) => a.title.localeCompare(b.title));
+      break;
+    case SortByEnum.Cheap:
+      products.sort((a, b) => a.price - b.price);
+      break;
+    case SortByEnum.Expensive:
+      products.sort((a, b) => b.price - a.price);
+      break;
+    case SortByEnum.Default:
+    default:
+      break;
+  }
 
   const getTitle = () => {
     if (category) {
@@ -42,6 +71,10 @@ export function ProductsPage() {
     }
 
     return '';
+  };
+
+  const handleSortChange = e => {
+    setSortBy(e.target.value);
   };
 
   return (
@@ -54,9 +87,36 @@ export function ProductsPage() {
         <Wrapper>
           <StyledPaper>
             <StyledDiv>
-              <Typography variant="subtitle1" gutterBottom>
-                {getTitle()}
-              </Typography>
+              <TitleDiv>
+                <Typography variant="subtitle1" gutterBottom>
+                  {getTitle()}
+                </Typography>
+
+                <SortBy>
+                  <InputLabel id="sortLabel">
+                    {t('products.sorting.title')}
+                  </InputLabel>
+                  <Select
+                    labelId="sortLabel"
+                    value={sortBy}
+                    onChange={handleSortChange}
+                  >
+                    <MenuItem value={SortByEnum.Default}>
+                      {t('products.sorting.none')}
+                    </MenuItem>
+                    <MenuItem value={SortByEnum.Alphabetical}>
+                      {t('products.sorting.alphabetical')}
+                    </MenuItem>
+                    <MenuItem value={SortByEnum.Expensive}>
+                      {t('products.sorting.expensive')}
+                    </MenuItem>
+                    <MenuItem value={SortByEnum.Cheap}>
+                      {t('products.sorting.cheap')}
+                    </MenuItem>
+                  </Select>
+                </SortBy>
+              </TitleDiv>
+
               <Grid container spacing={1}>
                 <Grid item xs={12}>
                   <Grid container justify="flex-start" spacing={4}>
@@ -79,7 +139,9 @@ export function ProductsPage() {
                 </Grid>
               </Grid>
 
-              {products.length === 0 && <Typography>No results.</Typography>}
+              {products.length === 0 && (
+                <Typography>{t('products.noResults')}</Typography>
+              )}
             </StyledDiv>
           </StyledPaper>
         </Wrapper>
@@ -96,6 +158,18 @@ const StyledPaper = styled(Paper)`
 const StyledDiv = styled.div`
   flex-grow: 1;
   padding: 2em;
+`;
+
+const TitleDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 1em;
+`;
+
+const SortBy = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 3em;
 `;
 
 const StyledLink = styled(Link)`
